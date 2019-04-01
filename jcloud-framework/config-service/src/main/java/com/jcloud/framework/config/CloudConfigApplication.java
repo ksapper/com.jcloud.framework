d5313522c41363f028161ce515d6e22b9900853a
mawanli <594212955@qq.com>
2019-04-01 10:22:55 +0800

    2.3.1 spring cloud 版本application不用注入@EnableConfigService

--- jcloud-framework/config-service/src/main/java/com/jcloud/framework/config/CloudConfigApplication.java
+++ jcloud-framework/config-service/src/main/java/com/jcloud/framework/config/CloudConfigApplication.java
@@ -2,10 +2,8 @@ package com.jcloud.framework.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class CloudConfigApplication {
 
     public static void main(String[] args) {
