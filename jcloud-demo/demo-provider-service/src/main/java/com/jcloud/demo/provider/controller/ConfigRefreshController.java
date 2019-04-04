132ea0c4430d980ed40c4cf6d82ebf4a74389b24
mawanli <594212955@qq.com>
2019-04-04 16:22:09 +0800

    demo provider

---
+++ jcloud-demo/demo-provider-service/src/main/java/com/jcloud/demo/provider/controller/ConfigRefreshController.java
@@ -0,0 +1,28 @@
+package com.jcloud.demo.provider.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: mwl
+ */
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigRefreshController {
+
+    @Value(value="${girl.age:}")
+    private String girlAge;
+
+    /**
+     * girl
+     * @return
+     */
+    @GetMapping("/configdate")
+    public String  loadUser() {
+        return girlAge;
+    }
+}
