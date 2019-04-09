717e55878c3ca7e62ff5c731bbbd90e6b55a3b60
mawanli <594212955@qq.com>
2019-04-09 11:52:52 +0800

    demo

---
+++ jcloud-demo/simple-consumer-movie/src/main/java/com/jcloud/framework/movie/persist/po/User.java
@@ -0,0 +1,17 @@
+package com.jcloud.framework.movie.persist.po;
+
+import lombok.Data;
+
+/**
+ * @Author: cy
+ */
+@Data
+public class User {
+
+    String userName;
+
+    String displayName;
+
+    String phone;
+
+}
