046ff994da106f718dac82cbee2e8f0183a1968b
mawanli <594212955@qq.com>
2019-04-09 17:35:01 +0800

    demo

--- jcloud-demo/simple-consumer-movie/src/main/java/com/jcloud/framework/movie/SimpleConsumerMovieApplication.java
+++ jcloud-demo/simple-consumer-movie/src/main/java/com/jcloud/framework/movie/SimpleConsumerMovieApplication.java
@@ -3,14 +3,18 @@ package com.jcloud.framework.movie;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan(basePackages = {"com.jcloud.framework.movie.persist.mapper"})
 public class SimpleConsumerMovieApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return  new RestTemplate();
     }
